25c471119ea5b20dbc576c5f5afb783560e8b785
Drakepal <playonplay23@gmail.com>
2022-09-29 13:32:47 -0500

    Letter is changing based on title

--- MyFavoriteBooks/View/BookView.swift
+++ MyFavoriteBooks/View/BookView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 extension Book {
+    
     struct Image: View {
+        
+        let title: String
+        
         var body: some View {
-            SwiftUI.Image(systemName: "books.vertical")
+            let symbol = SwiftUI.Image(title: title)
+            ??
+                .init(systemName: "books.vertical")
+            
+            
+            symbol
                 .resizable()
                 .scaledToFit()
                 .frame(width: 80, height: 80)
                 .foregroundColor(.secondary.opacity(0.5))
         }
     }
-    
+}
+extension Image {
+    init?(title: String) {
+        guard let character = title.first,
+        case let symbolName = "\(character.lowercased()).square",
+              UIImage(systemName: symbolName) != nil
+        else {
+            return nil
+        }
+       
+        self.init(systemName: symbolName)
+    }
+}
+
+
+struct Book_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Book.Image(title: Book().title)
+            Book.Image(title: "")
+            Book.Image(title: "📖")
+        }
+    }
 }
