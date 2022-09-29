43b8c74663aeb02ade866724efc5741c66eb83a8
Drakepal <playonplay23@gmail.com>
2022-09-29 12:26:33 -0500

    first commit

---
+++ MyFavoriteBooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFavoriteBooks
+//
+//  Created by PM Student on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFavoriteBooks/MyFavoriteBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteBooksApp.swift
+//  MyFavoriteBooks
+//
+//  Created by PM Student on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
